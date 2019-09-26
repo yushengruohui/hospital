fa51436007c5b5b4217711e222c29b574915cf4c
yusheng <1950249908@qq.com>
2019-09-26 16:12:19 +0800

    完成基本的springmvc搭建

--- hospital/src/main/java/com/ys/hospital/HospitalApplication.java
+++ hospital/src/main/java/com/ys/hospital/HospitalApplication.java
@@ -1,8 +1,15 @@
 package com.ys.hospital;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动器类
+ *
+ * @author yusheng
+ */
+@MapperScan("com.ys.hospital.dao")
 @SpringBootApplication
 public class HospitalApplication {
 
